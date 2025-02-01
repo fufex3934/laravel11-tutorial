a74f4076de47ac899caa80e77fd0010bf4f9a7ba
fufex3934 <fufawakgari174@gmail.com>
2025-02-01 15:12:09 +0300

    balde

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about/{name}', function ($name) {
+    return view('about',['name' => $name]);
+});
+
+Route::get('/home', function () {
+    return view('about');
+});
+
