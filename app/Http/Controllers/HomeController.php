02e505c5a34323a05da4e1ae0bdcee1bd4cb6768
fufex3934 <fufawakgari174@gmail.com>
2025-02-04 11:22:29 +0300

    route group with prefix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function show(){
-        return to_route('hm');
+        return 'Show students';
+    }
+    public function add(){
+        return 'add students';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('home/profile/user','home')->name('hm');
-Route::get('show',[HomeController::class,'show']);
+Route::view('home/profile/user', 'home')->name('hm');
+Route::get('show', [HomeController::class, 'show']);
+
+//route group with prefix
+Route::prefix('student')->group(function () {
+    Route::view('home', 'home');
+    Route::get('show', [HomeController::class, 'show']);
+    Route::get('add', [HomeController::class, 'add']);
+});
