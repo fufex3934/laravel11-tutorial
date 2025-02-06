4a10d55dd49b95465be97b8a944dff6367191185
fufex3934 <fufawakgari174@gmail.com>
2025-02-06 15:59:33 +0300

    elequent model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\DB;
 
-use function Pest\Laravel\get;
+
 
 class UserController extends Controller
 {
@@ -18,20 +19,7 @@ class UserController extends Controller
     }
     public function queries()
     {
-        // $response = DB::table('users')->get();
-        // $response = DB::table('users')->where('password', '1234')->get();
-        // $response  = DB::table('users')->where('name','fufa')->update(['password'=>'123']);
-        // if($response){
-        //     return "User updated successfully";
-        // }else{
-        //     return "Failed to update user";
-        // }
-        //  return view('users', ['users' => $response]);
-        $response  = DB::table('users')->where('name', 'fufa')->delete();
-        if ($response) {
-            return "User deleted successfully";
-        } else {
-            return "Failed to delete user";
-        }
+        $response = User::get();
+        return view('users',['users' => $response]);
     }
 }
